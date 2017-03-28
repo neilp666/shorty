645258b5f7d789fe521daf39e72e3b537699929d
neil patel <neil@myindianweddingapp.com>
2017-03-28 20:40:38 +0100

    Fixed issues with heroku

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,6 +6,6 @@ class Url < ApplicationRecord
 
   def get_short_url
     chars = ['0'..'9','A'..'Z','a'..'z'].map{|range| range.to_a}.flatten
-    self.short_url = 6.times.map{chars.sample}.join until Url.find_by_short_url(self.short_url).nil?
+    self.short_url = 6.times.map{chars.sample}.join
   end
 end
